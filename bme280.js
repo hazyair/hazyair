21decf63c3b68e621f67aa86d5c8acf5f05f2e21
marcin-sielski <marcin.sielski@gmail.com>
2018-01-12 23:31:58 +0100

    Bug fixes in bme280 module.

--- bme280.js
+++ bme280.js
@@ -1,11 +1,13 @@
+'use strict';
+
 const BME280Sensor = require('bme280-sensor');
 
 let init = false;
 
-BME280 = function(options) {
-    this.bme280 = new BME280(options);
+const BME280 = function(options) {
+    this.bme280 = new BME280Sensor(options);
     if (init === false) {
-        bme280.init().then((data) => {
+        this.bme280.init().then((data) => {
             init = true;
         }).catch((err) => {
             console.err(err);
@@ -16,7 +18,7 @@ BME280 = function(options) {
 
 BME280.prototype.temperature = function() {
     return new Promise((resolve, reject) => {
-        bme280.readSensorData().then((data) => {
+        this.bme280.readSensorData().then((data) => {
             return resolve({ 'temperature': { 'value': data.temperature_C, 'unit': '°C'},
             'model': 'BME280', 'timestamp': Date.now() });
         }).catch((err) => {
@@ -27,7 +29,7 @@ BME280.prototype.temperature = function() {
 
 BME280.prototype.pressure = function() {
     return new Promise((resolve, reject) => {
-        bme280.init().then((data) => {
+        this.bme280.init().then((data) => {
             return resolve({ 'pressure': { 'value': data.pressure_hPa, 'unit': 'hPa' },
             'model': 'BME280', 'timestamp': Date.now() });
         }).catch((err) => {
@@ -39,7 +41,7 @@ BME280.prototype.pressure = function() {
 
 BME280.prototype.humidity = function() {
     return new Promise((resolve, reject) => {
-        bme280.init().then((data) => {
+        this.bme280.init().then((data) => {
             return resolve({ 'humidity': { 'value': data.humidity, 'unit': '%' },
             'model': 'BME280', 'timestamp': Date.now() });
         }).catch((err) => {
