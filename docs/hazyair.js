73065be47c92cdb9fea1a3e1558331f29d42caf6
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-09 22:20:15 +0100

    Update wep page.

--- docs/hazyair.js
+++ docs/hazyair.js
@@ -1,13 +1,16 @@
 /*global dweetio*/
+/*global navigator*/
 
-function handler(dweet) {
+function listenHandler(dweet) {
 
     Object.keys(dweet.content).forEach(function(parameter) {
         document.getElementById(parameter).innerHTML = dweet.content[parameter];
         if (parameter === 'PM2.5Concentration' && parseInt(dweet.content[parameter], 10) > 25) {
             document.getElementById(parameter+'Text').className = 'hazyair-alert';
+            navigator.vibrate(200);
         } else if (parameter === 'PM10Concentration' && parseInt(dweet.content[parameter], 10) > 50) {
             document.getElementById(parameter+'Text').className = 'hazyair-alert';
+            navigator.vibrate(200);
         } else {
             document.getElementById(parameter+'Text').className = 'hazyair-result';
         }
@@ -15,12 +18,16 @@ function handler(dweet) {
     
 }
 
-dweetio.get_latest_dweet_for('25935C0E2C7F42558309E27E216C1D65', function(error, dweet) {
-
+function latestHandler(error, dweet) {
     if (error) return;
     dweet = dweet[0];
-    handler(dweet);
+    listenHandler(dweet);
+}
 
-});
+dweetio.get_latest_dweet_for('25935C0E2C7F42558309E27E216C1D65', latestHandler);
 
-dweetio.listen_for('25935C0E2C7F42558309E27E216C1D65', handler);
+dweetio.listen_for('25935C0E2C7F42558309E27E216C1D65', listenHandler);
+
+dweetio.get_latest_dweet_for('D47A7D484C1A41D2A1C33CDCBB9936ED', latestHandler);
+
+dweetio.listen_for('D47A7D484C1A41D2A1C33CDCBB9936ED', listenHandler);
