ad2800d57e1720c1254838dd0c0de2f43d096cce
Marcin Sielski <marcin.sielski@gmail.com>
2018-02-10 22:35:36 +0100

    Update wep page.

--- docs/hazyair.js
+++ docs/hazyair.js
@@ -102,7 +102,7 @@ function handleResize() {
 if ('serviceWorker' in navigator) {
   // Register a service worker hosted at the root of the
   // site using the default scope.
-  navigator.serviceWorker.register('hazyair.js').then(function(registration) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
     console.log('Service worker registration succeeded:', registration);
   }).catch(function(error) {
     console.log('Service worker registration failed:', error);
